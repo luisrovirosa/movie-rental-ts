66ddc69b288727362e656a1f7296e1cce3342b78
Luis Rovirosa Mairlot <luis@codium.team>
2024-11-15 22:30:28 +0100

    use template strings in formatters

--- src/consoleFormatter.ts
+++ src/consoleFormatter.ts
@@ -5,15 +5,15 @@ import {Rental} from "./rental";
 
 export class ConsoleFormatter implements Formatter {
     format(customer: Customer, rentals: Rentals): string {
-        let result = "Rental Record for " + customer.getName() + "\n";
+        let result = `Rental Record for ${customer.getName()}\n`;
 
         rentals.map((rental: Rental) => {
             result +=
-                "\t" + rental.getTitle() +
-                "\t" + rental.getPrice().toFixed(1) + "\n";
+                `\t${rental.getTitle()}` +
+                `\t${rental.getPrice().toFixed(1)}\n`;
         })
-        result += "Amount owed is " + rentals.getTotalAmount().toFixed(1) + "\n";
-        result += "You earned " + rentals.getFrequentRenterPoints() + " frequent renter points";
+        result += `Amount owed is ${rentals.getTotalAmount().toFixed(1)}\n`;
+        result += `You earned ${rentals.getFrequentRenterPoints()} frequent renter points`;
 
         return result;
 
