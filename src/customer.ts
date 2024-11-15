4a1946836c86b6197d64b9031f2d174fccac36bf
Luis Rovirosa Mairlot <luis@codium.team>
2024-11-15 19:31:09 +0100

    ignore reformat

--- src/customer.ts
+++ src/customer.ts
@@ -2,32 +2,32 @@ import {Rental} from "./rental";
 import {ConsoleFormatter} from "./consoleFormatter";
 
 export class Customer {
-  private name: string;
-  private rentals: Rental[] = [];
+    private name: string;
+    private rentals: Rental[] = [];
 
-  public constructor(name: string) {
-    this.name = name;
-  }
+    public constructor(name: string) {
+        this.name = name;
+    }
 
-  public addRental(arg: Rental) {
-    this.rentals.push(arg);
-  }
+    public addRental(arg: Rental) {
+        this.rentals.push(arg);
+    }
 
-  public getName(): string {
-    return this.name;
-  }
+    public getName(): string {
+        return this.name;
+    }
 
-  public statement(): string {
-    return new ConsoleFormatter().format(this);
-     }
+    public statement(): string {
+        return new ConsoleFormatter().format(this);
+    }
 
-  public getFrequentRenterPoints() {
-    return this.rentals.reduce((acc, rental) => acc + rental.getFrequentPoints(), 0);
-  }
+    public getFrequentRenterPoints() {
+        return this.rentals.reduce((acc, rental) => acc + rental.getFrequentPoints(), 0);
+    }
 
-  public getTotalAmount() {
-    return this.rentals.reduce((acc, rental) => acc + rental.getPrice(), 0);
-  }
+    public getTotalAmount() {
+        return this.rentals.reduce((acc, rental) => acc + rental.getPrice(), 0);
+    }
 
     getRentals() {
         return this.rentals;
