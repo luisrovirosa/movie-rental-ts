444b87916fc0c1f151a38b995af0f26b87bc0c98
Luis Rovirosa Mairlot <luis@codium.team>
2024-11-15 22:49:56 +0100

    extract price interface

--- src/newReleasePrice.ts
+++ src/newReleasePrice.ts
@@ -1,4 +1,6 @@
-export class NewReleasePrice {
+import {Price} from "./price";
+
+export class NewReleasePrice implements Price {
     priceFor(daysRented: number) {
         return daysRented * 3;
     }
--- src/regularPrice.ts
+++ src/regularPrice.ts
@@ -1,4 +1,6 @@
-export class RegularPrice {
+import {Price} from "./price";
+
+export class RegularPrice implements Price {
     priceFor(daysRented: number) {
         return daysRented <= 2 ? 2 : 2 + (daysRented - 2) * 1.5;
     }
