a475c0a0b56f9e8a957cdf3899d4e892de049110
Luis Rovirosa Mairlot <luis@codium.team>
2024-11-15 23:20:42 +0100

    change rentals to real map

--- src/formatter/consoleFormatter.ts
+++ src/formatter/consoleFormatter.ts
@@ -7,11 +7,10 @@ export class ConsoleFormatter implements Formatter {
     format(customer: Customer, rentals: Rentals): string {
         let result = `Rental Record for ${customer.getName()}\n`;
 
-        rentals.map((rental: Rental) => {
-            result +=
-                `\t${rental.getTitle()}` +
-                `\t${rental.getPrice().toFixed(1)}\n`;
-        })
+        result += rentals.map((rental: Rental): string =>
+            `\t${rental.getTitle()}` +
+            `\t${rental.getPrice().toFixed(1)}\n`)
+            .join('');
         result += `Amount owed is ${rentals.getTotalAmount().toFixed(1)}\n`;
         result += `You earned ${rentals.getFrequentRenterPoints()} frequent renter points`;
 
--- src/formatter/htmlFormatter.ts
+++ src/formatter/htmlFormatter.ts
@@ -8,9 +8,8 @@ export class HtmlFormatter implements Formatter {
         let result = `<h1>Rental Record for <em>${customer.getName()}</em></h1>\n` +
             '<table>\n';
 
-        rentals.map((rental: Rental) => {
-            result += `  <tr><td>${rental.getTitle()}</td><td>${rental.getPrice().toFixed(1)}</td></tr>\n`;
-        })
+        result += rentals.map((rental: Rental): string => `  <tr><td>${rental.getTitle()}</td><td>${rental.getPrice().toFixed(1)}</td></tr>\n`)
+            .join('');
         result += '</table>\n';
         result += `<p>Amount owed is <em>${rentals.getTotalAmount().toFixed(1)}</em></p>\n`;
         result += `<p>You earned <em>${rentals.getFrequentRenterPoints()}</em> frequent renter points</p>`;
