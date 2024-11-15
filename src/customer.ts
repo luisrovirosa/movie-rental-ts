80bcfee1fd8c6ae2f3c862e1c53dd5bd0f178664
Luis Rovirosa Mairlot <luis@codium.team>
2024-11-15 19:17:37 +0100

    extract getPrice inside customer

--- src/customer.ts
+++ src/customer.ts
@@ -1,6 +1,27 @@
 import { Movie } from "./movie";
 import { Rental } from "./rental";
 
+function getPrice(rental: Rental, thisAmount: number) {
+  switch (rental.getMovie().getPriceCode()) {
+    case Movie.REGULAR:
+      thisAmount += 2;
+      if (rental.getDaysRented() > 2) {
+        thisAmount += (rental.getDaysRented() - 2) * 1.5;
+      }
+      break;
+    case Movie.NEW_RELEASE:
+      thisAmount += rental.getDaysRented() * 3;
+      break;
+    case Movie.CHILDRENS:
+      thisAmount += 1.5;
+      if (rental.getDaysRented() > 3) {
+        thisAmount += (rental.getDaysRented() - 3) * 1.5;
+      }
+      break;
+  }
+  return thisAmount;
+}
+
 export class Customer {
   private name: string;
   private rentals: Rental[] = [];
@@ -26,23 +47,7 @@ export class Customer {
       let thisAmount = 0;
 
       // determine amounts for each line
-      switch (rental.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (rental.getDaysRented() > 2) {
-            thisAmount += (rental.getDaysRented() - 2) * 1.5;
-          }
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += rental.getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (rental.getDaysRented() > 3) {
-            thisAmount += (rental.getDaysRented() - 3) * 1.5;
-          }
-          break;
-      }
+      thisAmount = getPrice(rental, thisAmount);
 
       // add frequent renter points
       frequentRenterPoints++;
