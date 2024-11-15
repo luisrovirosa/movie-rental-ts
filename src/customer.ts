fd29faabc35722f60d4b1f4916c64cb899e1ce79
Luis Rovirosa Mairlot <luis@codium.team>
2024-11-15 19:19:50 +0100

    first step of move method

--- src/customer.ts
+++ src/customer.ts
@@ -2,25 +2,7 @@ import { Movie } from "./movie";
 import { Rental } from "./rental";
 
 function getPrice(rental: Rental) {
-  let thisAmount = 0;
-  switch (rental.getMovie().getPriceCode()) {
-    case Movie.REGULAR:
-      thisAmount = 2;
-      if (rental.getDaysRented() > 2) {
-        thisAmount += (rental.getDaysRented() - 2) * 1.5;
-      }
-      break;
-    case Movie.NEW_RELEASE:
-      thisAmount = rental.getDaysRented() * 3;
-      break;
-    case Movie.CHILDRENS:
-      thisAmount = 1.5;
-      if (rental.getDaysRented() > 3) {
-        thisAmount += (rental.getDaysRented() - 3) * 1.5;
-      }
-      break;
-  }
-  return thisAmount;
+  return rental.getPrice(rental);
 }
 
 export class Customer {
--- src/rental.ts
+++ src/rental.ts
@@ -17,4 +17,25 @@ export class Rental {
     public getMovie(): Movie {
         return this.movie;
     }
+    public getPrice(rental: Rental) {
+        let thisAmount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount = 2;
+                if (rental.getDaysRented() > 2) {
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount = rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount = 1.5;
+                if (rental.getDaysRented() > 3) {
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
 }
