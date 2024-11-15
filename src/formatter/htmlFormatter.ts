5f2f715790833228fb24611819f862d9a39f9666
Luis Rovirosa Mairlot <luis@codium.team>
2024-11-15 23:25:53 +0100

    htmlformatter 'simplified'

--- src/formatter/htmlFormatter.ts
+++ src/formatter/htmlFormatter.ts
@@ -5,15 +5,16 @@ import {Rental} from "../rental";
 
 export class HtmlFormatter implements Formatter {
     format(customer: Customer, rentals: Rentals): string {
-        let result = `<h1>Rental Record for <em>${customer.getName()}</em></h1>\n` +
-            '<table>\n';
+        let formattedRentals = rentals.map((rental: Rental): string => `  <tr><td>${rental.getTitle()}</td><td>${rental.getPrice().toFixed(1)}</td></tr>\n`);
 
-        result += rentals.map((rental: Rental): string => `  <tr><td>${rental.getTitle()}</td><td>${rental.getPrice().toFixed(1)}</td></tr>\n`)
-            .join('');
-        result += '</table>\n';
-        result += `<p>Amount owed is <em>${rentals.getTotalAmount().toFixed(1)}</em></p>\n`;
-        result += `<p>You earned <em>${rentals.getFrequentRenterPoints()}</em> frequent renter points</p>`;
-
-        return result;
+        let results = [
+            `<h1>Rental Record for <em>${customer.getName()}</em></h1>\n` +
+            `<table>\n`,
+            ...formattedRentals,
+            `</table>\n`,
+            `<p>Amount owed is <em>${rentals.getTotalAmount().toFixed(1)}</em></p>\n`,
+            `<p>You earned <em>${rentals.getFrequentRenterPoints()}</em> frequent renter points</p>`
+        ];
+        return results.join('');
     }
 }
