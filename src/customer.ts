1692a46970d68ced5c4bfdd4765ab2e4bc5ce5e1
Luis Rovirosa Mairlot <luis@codium.team>
2024-11-16 00:38:12 +0100

    refactor: renames variable and deletes unused function

--- src/customer.ts
+++ src/customer.ts
@@ -1,65 +1,72 @@
-import {Movie} from "./movie";
-import {Rental} from "./rental";
+import { Movie } from "./movie";
+import { Rental } from "./rental";
 
 export class Customer {
+  private name: string;
+  private rentals: Rental[] = [];
 
-    private name: string;
-    private rentals: Rental[] = [];
+  public constructor(name: string) {
+    this.name = name;
+  }
 
-    public constructor(name: string) {
-        this.name = name;
+  public addRental(arg: Rental) {
+    this.rentals.push(arg);
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public statement(): string {
+    let totalAmount: number = 0;
+    let frequentRenterPoints: number = 0;
+    let result = "Rental Record for " + this.getName() + "\n";
+
+    for (const rental of this.rentals) {
+      let thisAmount = 0;
+
+      // determine amounts for each line
+      switch (rental.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+          thisAmount += 2;
+          if (rental.getDaysRented() > 2) {
+            thisAmount += (rental.getDaysRented() - 2) * 1.5;
+          }
+          break;
+        case Movie.NEW_RELEASE:
+          thisAmount += rental.getDaysRented() * 3;
+          break;
+        case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (rental.getDaysRented() > 3) {
+            thisAmount += (rental.getDaysRented() - 3) * 1.5;
+          }
+          break;
+      }
+
+      // add frequent renter points
+      frequentRenterPoints++;
+      // add bonus for a two day new release rental
+      if (
+        rental.getMovie().getPriceCode() === Movie.NEW_RELEASE &&
+        rental.getDaysRented() > 1
+      )
+        frequentRenterPoints++;
+
+      // show figures for this rental
+      result +=
+        "\t" +
+        rental.getMovie().getTitle() +
+        "\t" +
+        thisAmount.toFixed(1) +
+        "\n";
+      totalAmount += thisAmount;
     }
 
-    public addRental(arg: Rental) {
-        this.rentals.push(arg);
-    }
+    // add footer lines
+    result += "Amount owed is " + totalAmount.toFixed(1) + "\n";
+    result += "You earned " + frequentRenterPoints + " frequent renter points";
 
-    public getName(): string {
-        return this.name;
-    }
-
-    public statement(): string {
-        let totalAmount: number = 0;
-        let frequentRenterPoints: number = 0;
-        let result = "Rental Record for " + this.getName() + "\n";
-
-        for (const each of this.rentals) {
-            let thisAmount = 0;
-
-            // determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
-
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() === Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount.toFixed(1) + "\n";
-            totalAmount += thisAmount;
-        }
-
-        // add footer lines
-        result += "Amount owed is " + totalAmount.toFixed(1) + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points";
-
-        return result;
-    }
+    return result;
+  }
 }
